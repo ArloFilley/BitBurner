b2f16204bf0a3e93a4610f6026b9701fa861bf59
ArloFilley <arlofilley@gmail.com>
2023-02-04 17:26:57 +0000

    improved installer

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 /** @param {NS} ns */
 export async function main(ns) {
+    await ns.wget("https://raw.githubusercontent.com/arlofilley/bitburner/master/wipefiles.js", "/utilities/wipefiles.js");
+    await ns.run("/utilities/wipefiles.js");
+
     await ns.wget("https://raw.githubusercontent.com/arlofilley/bitburner/master/main.js", "main.js");
     await ns.wget("https://raw.githubusercontent.com/arlofilley/bitburner/master/lib.js", "lib.js");
-    await ns.wget("https://raw.githubusercontent.com/arlofilley/bitburner/master/wipefiles.js", "/utilities/wipefiles.js");
 }
