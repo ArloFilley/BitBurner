56040820b714b96772a8b5fb3193456fe74e4789
ArloFilley <arlofilley@gmail.com>
2023-02-04 13:46:01 +0000

    added lib.js to install script

--- install.js
+++ install.js
@@ -1,4 +1,5 @@
 /** @param {NS} ns */
 export async function main(ns) {
     await ns.wget("https://raw.githubusercontent.com/arlofilley/bitburner/master/main.js", "main.js");
+    await ns.wget("https://raw.githubusercontent.com/arlofilley/bitburner/master/lib.js", "lib.js");
 }
